4d535a46244738ca1666f68eee5215e3acf7422e
Leandro Quipildor <83482016+LeooQuipildor@users.noreply.github.com>
2023-03-10 16:57:29 -0300

    Agregue el ejercicio 1 a la rama Arrays

---
+++ js/1-Arrays.js
@@ -0,0 +1,7 @@
+let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+let longitud = meses.length;
+
+for (let i = 0; i < longitud; i++) {
+    document.write(meses[i])
+    document.write(`<br>`)
+}
