d0abb1f1e983f11555c96ba2512de435fb6470ec
Leandro Quipildor <83482016+LeooQuipildor@users.noreply.github.com>
2023-03-11 16:28:48 -0300

    Realice el ejercicio 1 de funciones esta todo OK

---
+++ js/1-Funciones.js
@@ -0,0 +1,19 @@
+const numero = parseInt(prompt(`Ingresar un numero`));
+
+function numParImpar(numero){
+    if(numero % 2 === 0){
+        document.write(`El numero es par`);
+    }
+    else{
+        document.write(`El numero es impar`);
+    }
+}
+
+
+
+if(!isNaN(numero)){
+    numParImpar(numero);
+}
+else{
+    document.write(`ERROR!, Ingresar unicamente numeros enteros!`);
+}
