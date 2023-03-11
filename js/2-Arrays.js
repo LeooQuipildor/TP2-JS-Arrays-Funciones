d9d7cc4fcef1ca6d35e53277622271958ca23597
Leandro Quipildor <83482016+LeooQuipildor@users.noreply.github.com>
2023-03-11 15:40:59 -0300

    Agregue el segundo item que dice mostrar los elementos 1ero, 3ero y ultimo al ejercicio 2 de arrays

--- js/2-Arrays.js
+++ js/2-Arrays.js
@@ -1,13 +1,30 @@
 let ciudades = [];
 let agregar = [];
+let primera = [];
+let tercera = [];
+let ultimo = [];
 
 do {
     ciudades = prompt();
     agregar.push(ciudades);
+    if(agregar[0]){
+        primera = agregar[0];
+    }
+    if(agregar[2]){
+        tercera = agregar[2];
+    }
+    ultimo= agregar[agregar.length-1];
 }
 while (confirm("Continuar ingresando ciudades?: ")) {
     document.write(agregar);
     document.write(`<br>`);
     document.write(`La longitud del arrays es de: ${agregar.length}`);
+    document.write(`<br>`);
+    document.write(`El item en la primera posicion es: ${primera}`);
+    document.write(`<br>`);
+    document.write(`El item en la tercera posicion es: ${tercera}`);
+    document.write(`<br>`);
+    document.write(`El item en la ultima posicion es: ${ultimo}`);
+    document.write(`<br>`);
 }
 
